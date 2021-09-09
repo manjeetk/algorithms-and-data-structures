a93369ad36fc01371ea95e4043c8f619d58b53b1
Manjeet <kaur.manjeet7@gmail.com>
2021-09-09 15:54:00 +1000

    Added length check for the strings

--- Anargam.js
+++ Anargam.js
@@ -8,6 +8,9 @@
 
 
 function anargam(str1, str2){
+    if(str1.length !== str2.length) {
+      return false
+    }
     return str1.toLowerCase().split("").sort().join("") === 
     str2.toLowerCase().split("").sort().join("")
 }
