c55a8747cda33960ceed6e5cef90c90795840382
Manjeet <kaur.manjeet7@gmail.com>
2021-09-15 09:52:10 +1000

    Added new method to find the length of longest word.

--- LongestWord.js
+++ LongestWord.js
@@ -1,18 +1,18 @@
-// Return the length longest word from a string
+// Return the length longest word from a string and ignore punctuations
 
 //Method 1 - using split(), map() and Math.max()
 function longestWord1(str) {
-    let words = str.split(' ')
+    let words = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(" ")
     let lengths = words.map(word => word.length)
     return Math.max(...lengths)
 }
 
-console.log(longestWord1("This is a great"))
+console.log(longestWord1("This is a awesome"))
 
 
 //Method 2 - using split(), sort() and length()
 function longestWord2(str) {
-    let words = str.split(' ')
+    let words = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(" ")
     let sortedWords = words.sort( (a, b) => b.length - a.length)
     return sortedWords[0].length
 }
@@ -22,7 +22,7 @@ console.log(longestWord2("This is a great"))
 
 //Method 3 - using split(), for loop and length()
 function longestWord3(str) {
-    let words = str.split(' ')
+    let words = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(" ")
     let longestWord = 0
     for(let i = 0; i < words.length; i++) {
 
@@ -33,24 +33,24 @@ function longestWord3(str) {
     return longestWord;
 }
 
-console.log(longestWord3("This is a great"));
+console.log(longestWord3("This is a fanatstic"));
 
 
   
 //Method 4 - using split(), reduce() and length()
   function longestWord4(str) {
-    var longestWord = str.split(' ').reduce((longest, currentWord) => {
+    var longestWord = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(" ").reduce((longest, currentWord) => {
       return currentWord.length > longest.length ? currentWord : longest
     }, "")
     return longestWord.length
   }
 
-  console.log(longestWord4("This is a great"));
+  console.log(longestWord4("This is amazing "));
 
 
 //Method 5 - using split() and forEach() 
   function longestWord5(str) {
-    var words = str.split(" ")
+    var words = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(" ")
     var longest = 0;
     words.forEach(word  => {
       return  longest < word.length ? longest = word.length : ''
@@ -58,4 +58,20 @@ console.log(longestWord3("This is a great"));
 
     return longest;
 }
-console.log(longestWord5("This is a great"));
+console.log(longestWord5("This is a inspiring"));
+
+//Method 6 -
+function longestWord6(str) { 
+    let strArray = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split(" ")
+    let largestLength = 0
+    for (let i= 0; i < strArray.length; i++) {
+        if (strArray[i].length > largestLength) {
+            largestLength = strArray[i].length
+        }
+    }
+  return largestLength
+}
+   
+console.log(longestWord6("This is a incredible"))
+
+
